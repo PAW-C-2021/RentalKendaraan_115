6c9989e7aa7c5da7a27a0133afb17668e25d8c9a
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2021-11-04 21:26:24 +0700

    Add project files.

---
+++ RentalKendaraan/Models/Kendaraan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan.Models
+{
+    public partial class Kendaraan
+    {
+        public Kendaraan()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdKendaraan { get; set; }
+        public string NamaKendaraan { get; set; }
+        public byte[] NoPolisi { get; set; }
+        public string NoStnk { get; set; }
+        public int? IdJenisKendaraan { get; set; }
+        public byte[] Ketersediaan { get; set; }
+
+        public JenisKendaraan IdJenisKendaraanNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
---
+++ RentalKendaraan/Models/KondisiKendaraan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan.Models
+{
+    public partial class KondisiKendaraan
+    {
+        public KondisiKendaraan()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+
+        public int IdKondisi { get; set; }
+        public byte[] NamaKondisi { get; set; }
+
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
